9e445edfbd8fe70680e43b832d02469fe9e32deb
George-Bac <george.bacalu@endava.com>
2022-09-26 15:10:18 +0300

    Create home page

--- app/src/main/java/com/endava/library/MainActivity.java
+++ app/src/main/java/com/endava/library/MainActivity.java
@@ -3,12 +3,25 @@ package com.endava.library;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+   private Button allBooksButton, currentlyReadingButton, alreadyReadButton, wishListButton, favoritesButton, aboutButton;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
+      initViews();
+   }
+
+   private void initViews() {
+      allBooksButton = findViewById(R.id.allBooksButton);
+      currentlyReadingButton = findViewById(R.id.currentlyReadingButton);
+      alreadyReadButton = findViewById(R.id.alreadyReadButton);
+      wishListButton = findViewById(R.id.wishListButton);
+      favoritesButton = findViewById(R.id.favoritesButton);
+      aboutButton = findViewById(R.id.aboutButton);
    }
 }
