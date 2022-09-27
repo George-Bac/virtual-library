3735487f61aa67d0a18638abd8762bd3550b03ca
George-Bac <george.bacalu@endava.com>
2022-09-27 08:13:23 +0300

    Add support action bar for navigating back and have a label that indicates the page we are on

--- app/src/main/java/com/endava/library/AllBooksActivity.java
+++ app/src/main/java/com/endava/library/AllBooksActivity.java
@@ -1,10 +1,14 @@
 package com.endava.library;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Objects;
 
 public class AllBooksActivity extends AppCompatActivity {
 
@@ -15,6 +19,7 @@ public class AllBooksActivity extends AppCompatActivity {
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_all_books);
+      Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
       allBooksRecyclerView = findViewById(R.id.allBooksRecyclerView);
       bookRecyclerViewAdapter = new BookRecyclerViewAdapter(this, "allBooks");
@@ -22,5 +27,13 @@ public class AllBooksActivity extends AppCompatActivity {
       bookRecyclerViewAdapter.setBooks(BookUtils.getAllBooks());
       allBooksRecyclerView.setAdapter(bookRecyclerViewAdapter);
       allBooksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+   }
+
+   @Override
+   public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+      if (item.getItemId() == android.R.id.home) {
+         onBackPressed();
+      }
+      return super.onOptionsItemSelected(item);
    }
 }
