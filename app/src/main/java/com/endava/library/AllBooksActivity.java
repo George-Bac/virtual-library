57535000cc27f7ba1ab5502e7e558d6bf35618f3
George-Bac <george.bacalu@endava.com>
2022-09-27 08:01:13 +0300

    Create a new style for animations in the themes.xml and apply the animations to all the activities

--- app/src/main/java/com/endava/library/AllBooksActivity.java
+++ app/src/main/java/com/endava/library/AllBooksActivity.java
@@ -16,19 +16,11 @@ public class AllBooksActivity extends AppCompatActivity {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_all_books);
 
-      overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
-
       allBooksRecyclerView = findViewById(R.id.allBooksRecyclerView);
       bookRecyclerViewAdapter = new BookRecyclerViewAdapter(this, "allBooks");
 
       bookRecyclerViewAdapter.setBooks(BookUtils.getAllBooks());
       allBooksRecyclerView.setAdapter(bookRecyclerViewAdapter);
       allBooksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-   }
-
-   @Override
-   public void finish() {
-      super.finish();
-      overridePendingTransition(R.anim.slide_out, R.anim.slide_in);
    }
 }
